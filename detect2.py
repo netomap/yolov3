d9b1dca961ad546a82f50499e1f375e0b5a393cf
Manuel <neto.map@gmail.com>
2020-10-18 19:19:53 -0300

    update2 detect2.py
    update

--- detect2.py
+++ detect2.py
@@ -8,14 +8,15 @@ import os
 import sys
 from models import *
 from PIL import Image, ImageFont, ImageDraw
-
+import matplotlib.pyplot as plt
 # descobrindo o device
-device = torch.device('gpu') if torch.cuda.is_available() else torch.device('cpu')
+#device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+device = torch.device('cpu')
 
 # iniciando o modelo
 imgsz = 416
-model_cfg = './cfg/yolov3-spp.cfg'
-model_weights = './weights/yolov3-spp.pt'
+model_cfg = '/content/yolov3/cfg/yolov3-tiny.cfg'
+model_weights = '/content/yolov3/weights/best.pt'
 model = Darknet(model_cfg, imgsz)
 
 model.load_state_dict(torch.load(model_weights, map_location=device)['model'])
@@ -24,18 +25,18 @@ model.load_state_dict(torch.load(model_weights, map_location=device)['model'])
 model.to(device).eval()
 
 # carregando nomes das classes
-class_file = open('./data/dataset.names', 'r')
+class_file = open('/content/yolov3/data/classes.names', 'r')
 classes = class_file.readlines()
 class_file.close()
 classes = [nome.strip() for nome in classes]
 print (classes)
 
-img_path = './data/samples/img6.jpg'
+img_path = '/content/yolov3/data/samples/teste4.jpg'
 img = cv2.imread(img_path)
 print ('tamanho original da imagem: {}'.format(img.shape))
 
-new_w = 512 #512 para spp ou 674 para tiny
-new_h = 316 #316 para spp ou 416 para tiny
+new_w = 674 #512 para spp ou 674 para tiny
+new_h = 416 #316 para spp ou 416 para tiny
 
 img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
 
@@ -64,9 +65,7 @@ pred = non_max_suppression(pred, 0.3, 0.6, multi_label=False, classes=None)
 # pegando de fato das detecções
 pred = pred[0]
 
-# e transformando em um vetor numpy
-detections = pred.detach().numpy()
-print ('detections: ', detections)
+
 
 def imprime_deteccoes(img_path, new_w, new_h, detections, classes, texto):
     img_pil = Image.open(img_path)
@@ -80,10 +79,18 @@ def imprime_deteccoes(img_path, new_w, new_h, detections, classes, texto):
     
     return img_pil
 
-texto = 'Detecção usando {} em {} segundos com {} detecções.'.format(model_cfg, tempo, len(detections))
-nova_imagem = imprime_deteccoes(img_path, new_w, new_h, detections, classes, texto)
-plt.imshow(nova_imagem)
-plt.xticks([])
-plt.yticks([])
-plt.xlabel(texto)
-plt.show()
+if (pred): # se o vetor pred tem alguma predição, se não é None, então
+
+  # e transformando em um vetor numpy
+  detections = pred.detach().numpy()
+  print ('detections: ', detections)
+
+  texto = 'Detecção usando {} em {} segundos com {} detecções.'.format(model_cfg, tempo, len(detections))
+  nova_imagem = imprime_deteccoes(img_path, new_w, new_h, detections, classes, texto)
+  plt.imshow(nova_imagem)
+  plt.xticks([])
+  plt.yticks([])
+  plt.xlabel(texto)
+  plt.show()
+else:
+  print ('não houve detecções de pedestres na foto.')
