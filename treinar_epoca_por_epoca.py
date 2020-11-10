fed777c8bb756a6e5e130b88489de2f520056b35
Manuel <neto.map@gmail.com>
2020-11-09 23:16:35 -0300

    update
    update para colab_branch

--- treinar_epoca_por_epoca.py
+++ treinar_epoca_por_epoca.py
@@ -2,17 +2,15 @@ import torch
 import os
 
 device = torch.device('cpu')
-caminho_pesos = '/home/manuel/PycharmProjects/yolov3/last.pt'
-wdir = ''
 
 for k in range(5):
 
     checkpoint = torch.load('/content/drive/My Drive/Colab Notebooks/YOLO/YOLO_PERSON_COCO_DATASET/last.pt', map_location=device)
     proxima_epoca = checkpoint['epoch'] + 2
     comando = "python3 train.py --cfg './cfg/yolov3-tiny.cfg' " \
-              "--wdir /content/drive/My Drive/Colab Notebooks/YOLO/YOLO_PERSON_COCO_DATASET/ " \
-              "--weights /content/drive/My Drive/Colab Notebooks/YOLO/YOLO_PERSON_COCO_DATASET/last.pt " \
-              "--data 'dataset.data' " \
+              "--wdir '/content/drive/My Drive/Colab Notebooks/YOLO/YOLO_PERSON_COCO_DATASET/' " \
+              "--weights '/content/drive/My Drive/Colab Notebooks/YOLO/YOLO_PERSON_COCO_DATASET/last.pt' " \
+              "--data '/content/dataset.data' " \
               "--epochs {} " \
               "--batch-size 1".format(proxima_epoca)
 
